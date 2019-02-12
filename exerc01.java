cbdca2b66a734c2ba677010db14847c8b5a676a6
adjamba <47555385+adjamba@users.noreply.github.com>
2019-02-12 10:27:15 +0100

    Add files via upload

---
+++ exerc01.java
@@ -0,0 +1,10 @@
+/*Estrutura de repitição
+ *
+ * */
+public class exerc01{
+  public static void main(String args[]){
+    for(int contar = 0;contar <10; contar ++){
+      System.out.println("O valor da variável contar é: "+contar);
+    }
+  }
+}
